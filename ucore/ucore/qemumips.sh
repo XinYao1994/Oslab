baf73d1fe005838141a480fc8c3b1503d2a78b51
xyao <authwork@163.com>
2016-04-07 11:42:01 +0800

    port to new board

--- ucore/ucore/qemumips.sh
+++ ucore/ucore/qemumips.sh
@@ -1,3 +1,3 @@
 ON_FPGA=n make ARCH=mips menuconfig
-ON_FPGA=n make ARCH=mips sfsimg > log
-ON_FPGA=n make ARCH=mips kernel >> log
+ON_FPGA=n make ARCH=mips sfsimg 2> log
+ON_FPGA=n make ARCH=mips kernel 2>> log
