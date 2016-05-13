4284e7893e60499212faf7421c1c1d602d9b0fc6
xyao <authwork@163.com>
2016-05-13 10:45:40 +0800

    i386 fixed

--- ucore/.config.cmd
+++ ucore/.config.cmd
@@ -3,7 +3,7 @@ deps_config := \
 	src/kern-ucore/schedule/Kconfig \
 	src/kern-ucore/mm/Kconfig \
 	src/kern-ucore/Kconfig \
-	/home/hust/git/ucore/src/kern-ucore/arch/mips/Kconfig
+	/home/hust/git/TinyComputer/ucore/src/kern-ucore/arch/i386/Kconfig
 
 include/config/auto.conf: \
 	$(deps_config)
--- ucore/src/libs-user-ucore/arch/i386/arch.h
+++ ucore/src/libs-user-ucore/arch/i386/arch.h
@@ -18,6 +18,36 @@
             __mod;                                                  \
         })
 
+static inline unsigned int __mulu10(unsigned int n)
+{
+	return (n << 3) + (n << 1);
+}
+
+/* __divu* routines are from the book, Hacker's Delight */
+
+static inline unsigned int __divu10(unsigned int n)
+{
+	unsigned int q, r;
+	q = (n >> 1) + (n >> 2);
+	q = q + (q >> 4);
+	q = q + (q >> 8);
+	q = q + (q >> 16);
+	q = q >> 3;
+	r = n - __mulu10(q);
+	return q + ((r + 6) >> 4);
+}
+
+static inline unsigned __divu5(unsigned int n)
+{
+	unsigned int q, r;
+	q = (n >> 3) + (n >> 4);
+	q = q + (q >> 4);
+	q = q + (q >> 8);
+	q = q + (q >> 16);
+	r = n - q * 5;
+	return q + (13 * r >> 6);
+}
+
 static inline uint8_t inb(uint16_t port) __attribute__ ((always_inline));
 static inline void insl(uint32_t port, void *addr, int cnt)
     __attribute__ ((always_inline));
